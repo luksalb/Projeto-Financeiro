28a3984a0cd0cd7b3ee7372df22d8390f1783f09
SMWE Consultoria <microsoft@smwe.com.br>
2023-09-24 04:56:31 -0300

    Add project files.

---
+++ ProjetoFinanceiro/PessoaFisica.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoFinanceiro
+{
+    public class PessoaFisica : Cliente
+    {
+        public PessoaFisica(string codigo, string nome) : base(codigo, nome)
+        {
+        }
+
+        public override decimal DescontarTaxa(decimal valor)
+        {
+            return valor - 1;
+        }
+    }
+}
---
+++ ProjetoFinanceiro/PessoaJuridica.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoFinanceiro
+{
+    public class PessoaJuridica : Cliente
+    {
+        public PessoaJuridica(string codigo, string nome) : base(codigo, nome)
+        {
+        }
+
+        public override decimal DescontarTaxa(decimal valor)
+        {
+            return valor - 2;
+        }
+    }
+
+}
